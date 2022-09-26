36e0d2a93897e9b291d7713b4e5804e9f45fc8fc
Tanveer Thethi <tanveert@ad.unc.edu>
2022-09-26 00:07:38 -0400

    fhfhf

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,11 +1,11 @@
-"""EX04 - 'List' Utility Functions.""" 
+"""EX05 - This is where function skeletons and implementations will be implemented.""" 
 
 
 __author__ = "730578741"
 
 
 def only_evens(num_list: list[int]) -> str:
-    """Returns all thhe even numbers from list"""
+    """Returns all the even numbers from list"""
     even_list: str = []
     i: int = 0
 
@@ -13,34 +13,39 @@ def only_evens(num_list: list[int]) -> str:
         if num_list[i] % 2 == 0:
             even_list.append(num_list[i])
         i += 1
-    
-    print(even_list)
+    return even_list
 
 
 def concat(list1: list[int], list2: list[int]) -> str:
     """Returns an int list that is made by adding the values of the second list immediately to the first list"""
+    if len(list1) == 0 and len(list1) == 0:
+        return []
+    else:
+        if len(list1) == 0:
+            return []
+        if len(list2) == 0:
+            return []
+
     i: int = 0
     while i < len(list2):
         list1.append(list2[i])
         i+=1
-    print(list1)
+    return list1
 
 
 def sub(num_list: list[int], start: int, end: int) -> int:
+    """Returns a list which is a subset of the given list, between the specified start index and the end index - 1"""
     sublist: str = []
 
     if start < 0:
         start = 0
-
     if end > len(num_list):
         end = len(num_list) - 1
-    
-    if len(num_list) == 0 or start > len(num_list) or end < 0:
-        print(sublist)
 
-    while start < end:
-        sublist.append(num_list[start])
-        start+=1
-    print(sublist)
-    
-    
+    if len(num_list) == 0 or start > len(num_list) or end < 0:
+        return sublist
+    else:
+        while start < end:
+            sublist.append(num_list[start])
+            start+=1
+        return sublist
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,64 @@
+"""EX05 - Unit Tests.""" 
+
+
+__author__ = "730578741"
+
+
+from utils import only_evens, sub, concat
+
+
+def test_only_evens_empty() -> None:
+    """Edge case testing for empty num_list. Should return []."""
+    num_list: list[int] = []
+    assert only_evens(num_list) == []
+
+
+def test_only_evens_odds_only() -> None:
+    """First use case testing for num_list with only odd numbers. Should return []."""
+    num_list: list[int] = [1,3,5,7,9,11,13,15]
+    assert only_evens(num_list) == []
+
+
+def test_only_evens_both() -> None:
+    """Second use case testing for num_list with numbers 1-10. Should return [2,4,6,8,10]."""
+    num_list: list[int] = [1,2,3,4,5,6,7,8,9,10]
+    assert only_evens(num_list) == [2,4,6,8,10]
+
+
+def test_sub_empty() -> None:
+    """Edge case testing for empty num_list. Should return []."""
+    num_list: list[int] = []
+    assert sub(num_list, 1, 3) == []
+
+
+def test_sub_first_normal1() -> None:
+    """First use case testing to make sure that 'sub' function works correctly. Should return [20, 30]."""
+    num_list = [10, 20, 30, 40]
+    assert sub(num_list, 1, 3) == [20, 30]
+
+
+def test_sub_second_normal() -> None:
+    """Second use case testing to make sure that 'sub' function works correctly. Should return [10, 20, 30]."""
+    num_list = [10, 20, 30, 40]
+    assert sub(num_list, -1, 3) == [10, 20, 30]
+
+
+def test_concat_empty() -> None:
+    """Edge case testing for empty num_list. Should return []."""
+    list1 = []
+    list2 = [4, 5, 6]
+
+    assert concat(list1, list2) == []
+
+def test_concat_normal1() -> None:
+    """First use case testing to make sure that 'conact' function works correctly. Should return [1, 2, 3, 4, 5, 6]."""
+    list1 = [1, 2, 3]
+    list2 = [4, 5, 6]
+    assert concat(list1 , list2) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_normal2() -> None:
+    """Second use case testing to make sure that 'conact' function works correctly. Should return [10, 20, 30, 40, 50, 60]."""
+    list1 = [10, 20, 30]
+    list2 = [40, 50, 60]
+    assert concat(list1 , list2) == [10, 20, 30, 40, 50, 60]
